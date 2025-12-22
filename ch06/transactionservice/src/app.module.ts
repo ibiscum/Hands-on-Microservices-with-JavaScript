f4183c2cef93853d2f7e487ae73d49ab0d239527
ibiscum <ulf.luecking@gmail.com>
2025-12-22 12:44:02 +0100

    update

--- ch06/transactionservice/src/app.module.ts
+++ ch06/transactionservice/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { PrismaModule } from './prisma/prisma.module.js';
+import { TransactionModule } from './transaction/transaction.module.js';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, TransactionModule],
   controllers: [AppController],
   providers: [AppService],
 })
